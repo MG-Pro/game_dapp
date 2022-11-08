bfa59919bc1a81a7d8b2aab7fd049a8252d193d2
mg <mg6625@gmail.com>
2022-11-08 20:51:37 +0300

    upd deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
+require('dotenv').config()
 import "@nomicfoundation/hardhat-toolbox";
+const {API_URL, PRIVATE_KEY, API_KEY} = process.env
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY as string]
+    }
+  }
 };
 
 export default config;
